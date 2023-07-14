8e04973003c727307c933f06ab9df6ce9d9e5759
ren666yyds666 <139411617+ren666yyds666@users.noreply.github.com>
2023-07-14 21:19:03 +0800

    Update 3.lua

--- 3.lua
+++ 3.lua
@@ -9,10 +9,6 @@ if a=="123fa98"then
 _G.bmd=true
 elseif a=="wgiAmhlb9"then
 _G.bmd=true
-if a=="123fa98"then
-_G.bmd=true
-elseif a=="wgiAmhlb9"then
-_G.bmd=true
 elseif a=="yaqshjd"then
 _G.bmd=true
 elseif a=="mahaoboma"then
@@ -165,127 +161,59 @@ elseif a=="qwert23569"then
 _G.bmd=true
 elseif a=="weshouldghb"then
 _G.bmd=true
-elseif a=="yaqshjd"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="mahaoboma"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="wdkdkkdsshgg"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="chinese36153"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="linnnnnnnsq"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="CN_Lin"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="yuanshenzmnl3"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="ewqqwemnbbnm123"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="qw7723qw7723/4n0therunl0"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="wjjejdhdhshshs"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="ttfkhv"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="yaortyucd"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="q8opw"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="Qwertyuiop1ih"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="djcodtv"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="4n0therunl0"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="djcodtv"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="xu6096"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="ducuycsb"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="yizi123123"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="hand14521"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="sijsksiskisj"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="zy66uk"then
-_G.bmd=true
-elseif a=="fudhfchdh"then
-_G.bmd=true
-elseif a=="sijsksiskisj"then
-_G.bmd=true
-elseif a=="123jdxvt"then
-_G.bmd=true
-elseif a=="whsaajj"then
-_G.bmd=true
-elseif a=="HHVVHVVF"then
-_G.bmd=true
-elseif a=="HRB_cat"then
-_G.bmd=true
-elseif a=="ndjdhdgddgdgyz"then
-_G.bmd=true
-elseif a=="ffjfjcjid"then
-_G.bmd=true
-elseif a=="ueuryry43"then
-_G.bmd=true
-elseif a=="zy66uk"then
-_G.bmd=true
-elseif a=="147258qazwsx5"then
-_G.bmd=true
-elseif a=="wweettyyit"then
+elseif a=="用户名"then
 _G.bmd=true
 elseif a=="He5vy"then
 _G.bmd=true
-elseif a=="seeksanren"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="uihcjk"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="CN_diuveikv"then
-_G.bmd=true
-elseif a=="qeasrdx"then
-_G.bmd=true
-elseif a=="widugrd"then
-_G.bmd=true
-elseif a=="cbh114514"then
-_G.bmd=true
-elseif a=="qwertyuiop91665"then
-_G.bmd=true
-elseif a=="uihcjk"then
-_G.bmd=true
-elseif a=="sksk534"then
-_G.bmd=true
-elseif a=="xu6096"then
-_G.bmd=true
-elseif a=="vdbdv7515"then
-_G.bmd=true
-elseif a=="12345qeert2"then
-_G.bmd=true
-elseif a=="nitam_adesb"then
-_G.bmd=true
-elseif a=="sanren34"then
-_G.bmd=true
-elseif a=="syrdgwkyb"then
-_G.bmd=true
-elseif a=="idhso99"then
-_G.bmd=true
-elseif a=="LYh21359"then
-_G.bmd=true
-elseif a=="syrdgwkyb"then
-_G.bmd=true
-elseif a=="www_123wwe"then
-_G.bmd=true
-elseif a=="lavecaxec"then
-_G.bmd=true
-elseif a=="qwerqwerqw7723"then
-_G.bmd=true
-elseif a=="zhan1689"then
-_G.bmd=true
-elseif a=="AT_TA"then
-_G.bmd=true
-elseif a=="v_v"then
-_G.bmd=true
-elseif a=="uahzjzhshs"then
+elseif a=="用户名"then
 _G.bmd=true
 elseif a=="用户名"then
 _G.bmd=true
@@ -293,7 +221,19 @@ elseif a=="用户名"then
 _G.bmd=true
 elseif a=="用户名"then
 _G.bmd=true
-elseif a=="bzbzwq"then
+elseif a=="用户名"then
+_G.bmd=true
+elseif a=="用户名"then
+_G.bmd=true
+elseif a=="用户名"then
+_G.bmd=true
+elseif a=="用户名"then
+_G.bmd=true
+elseif a=="用户名"then
+_G.bmd=true
+elseif a=="用户名"then
+_G.bmd=true
+elseif a=="用户名"then
 _G.bmd=true
 elseif a=="用户名"then
 _G.bmd=true
@@ -321,26 +261,24 @@ elseif a=="用户名"then
 _G.bmd=true
 elseif a=="用户名"then
 _G.bmd=true
-elseif a=="lvqwerty"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="123fesek"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="114514i9"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="ATXINGXIN66"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="123789qwerty9"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="zhan1689"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="zhan1689"then
+elseif a=="用户名"then
 _G.bmd=true
-elseif a=="qwert23569"then
-_G.bmd=true
-elseif a=="weshouldghb"then
+elseif a=="用户名"then
 _G.bmd=true
 end
-if _G.bmd==true then  
+if _G.bmd==true then
 local CoreGui = game:GetService("StarterGui")
 CoreGui:SetCore("SendNotification", {
     Title = "忍 脚 本",
@@ -348,7 +286,7 @@ CoreGui:SetCore("SendNotification", {
     Duration = 5,
 })
 loadstring(game:HttpGet(utf8.char((function() return table.unpack({104,116,116,112,115,58,47,47,114,97,119,46,103,105,116,104,117,98,117,115,101,114,99,111,110,116,101,110,116,46,99,111,109,47,114,101,110,54,54,54,121,121,100,115,54,54,54,47,118,105,97,110,120,106,119,114,110,120,107,115,108,120,117,45,97,107,120,106,119,115,120,107,113,110,100,120,120,106,120,99,97,106,100,99,47,109,97,105,110,47,50,46,108,117,97})end)())))();
-   else
+      else
   setclipboard("830835939")
   game.Players.LocalPlayer:Kick("请加830835939获取白名单")
 end
