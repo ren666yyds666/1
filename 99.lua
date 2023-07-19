a2238b25a60fbeaf65f0b356bfa0ada920eda406
ren666yyds666 <139411617+ren666yyds666@users.noreply.github.com>
2023-07-19 11:46:41 +0800

    Create 99.lua

---
+++ 99.lua
@@ -0,0 +1,50 @@
+local CoreGui = game:GetService("StarterGui")
+CoreGui:SetCore("SendNotification", {
+    Title = "忍 脚 本",
+    Text = "正在验证....",
+    Duration = 5,
+})
+local a=tostring(game.Players.LocalPlayer.Character);
+if a=="123fa98"then
+_G.bmd=true
+elseif a=="用户名"then
+_G.bmd=true
+elseif a=="用户名"then
+_G.bmd=true
+elseif a=="用户名"then
+_G.bmd=true
+elseif a=="用户名"then
+_G.bmd=true
+elseif a=="用户名"then
+_G.bmd=true
+elseif a=="用户名"then
+_G.bmd=true
+elseif a=="用户名"then
+_G.bmd=true
+elseif a=="用户名"then
+_G.bmd=true
+elseif a=="用户名"then
+_G.bmd=true
+elseif a=="用户名"then
+_G.bmd=true
+elseif a=="用户名"then
+_G.bmd=true
+elseif a=="用户名"then
+_G.bmd=true
+elseif a=="用户名"then
+_G.bmd=true
+elseif a=="用户名"then
+_G.bmd=true
+end
+if _G.bmd==true then
+local CoreGui = game:GetService("StarterGui")
+CoreGui:SetCore("SendNotification", {
+    Title = "忍 脚 本",
+    Text = a.."欢迎您",
+    Duration = 5,
+})
+loadstring(game:HttpGet("https://pastebin.com/raw/yWuNWVRh"))();
+      else
+  setclipboard("830835939")
+  game.Players.LocalPlayer:Kick("请加830835939获取白名单")
+end
