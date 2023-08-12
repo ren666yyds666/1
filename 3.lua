e09b1bb89d22eb67923eecc165e9087a0e924913
ren666yyds666 <139411617+ren666yyds666@users.noreply.github.com>
2023-08-12 18:18:18 +0800

    Update 3.lua

--- 3.lua
+++ 3.lua
@@ -1,89 +1,45 @@
-local ScreenGui = Instance.new("ScreenGui")
-local UI = Instance.new("Frame")
-local Title = Instance.new("TextLabel")
-local Frame = Instance.new("Frame")
-local Key = Instance.new("TextBox")
-local Start = Instance.new("TextButton")
 local a=tostring(game.Players.LocalPlayer.Character);
 
-ScreenGui.Parent = game.CoreGui
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-UI.Name = "卡密系统"
-UI.Parent = ScreenGui
-UI.Active = true
-UI.BackgroundColor3 = Color3.new(255, 255, 0)
-UI.BackgroundTransparency = 0
-UI.BorderSizePixel = 3
-UI.Position = UDim2.new(0.5, -150, 0.5, -67)
-UI.Size = UDim2.new(0, 260, 0, 250)
-UI.Draggable = true
-
-Title.Name = "Title"
-Title.Parent = UI
-Title.BackgroundColor3 = Color3.new(68, 68, 68)
-Title.BackgroundTransparency = 1
-Title.BorderSizePixel = 2
-Title.Position = UDim2.new(0, 0, 0.02, 0)
-Title.Size = UDim2.new(1, 0, 0, 50)
-Title.Font = Enum.Font.SourceSans
-Title.Text = "忍脚本卡密系统"
-Title.TextColor3 = Color3.new(0, 0, 0)
-Title.TextScaled = true
-Title.TextSize = 14
-Title.TextWrapped = true
-
-Frame.Parent = Title
-Frame.BackgroundColor3 = Color3.new(0, 0, 0)
-Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.07, 0, 0.9, 0)
-Frame.Size = UDim2.new(0.85, 0, 0, 6)
-
-Key.Name = "Key"
-Key.Parent = UI
-Key.BackgroundColor3 = Color3.new(255, 255, 255)
-Key.BorderSizePixel = 0
-Key.Position = UDim2.new(0.1, 0, 0.31, 0)
-Key.Size = UDim2.new(0.8, 0, 0, 50)
-Key.Font = Enum.Font.SourceSans
-Key.PlaceholderText = "请输入卡密"
-Key.Text = ""
-Key.TextColor3 = Color3.new(0, 0, 0)
-Key.TextScaled = true
-Key.TextSize = 14
-Key.TextWrapped = true
-
-Start.Name = "Start"
-Start.Parent = UI
-Start.BackgroundColor3 = Color3.new(255, 255, 255)
-Start.BackgroundTransparency = 0
-Start.BorderSizePixel = 2
-Start.Position = UDim2.new(0.25, 0, 0.65, 0)
-Start.Size = UDim2.new(0.5, 0, 0, 45)
-Start.Font = Enum.Font.Gotham
-Start.Text = "确定"
-Start.TextColor3 = Color3.new(0, 0, 0)
-Start.TextScaled = true
-Start.TextSize = 10
-Start.TextWrapped = true
-
-Start.MouseButton1Click:Connect(function()
-    if Key.Text == "China First" then
-        ScreenGui:Destroy()
-        local CoreGui = game:GetService("StarterGui")
-         CoreGui:SetCore("SendNotification", {
-           Title = "卡密正确",
-           Text = a.."，欢迎您",
-           Duration = 20,
-         })
+------------------------------------黑名单------------------------------------
+if a=="用户名"then
+_G.hmd=true 
+elseif a=="用户名"then
+_G.bmd=true
+if _G.hmd==true then
+game.Players.LocalPlayer:Kick(a.."，您已进入忍脚本黑名单")
+else
+------------------------------------白名单------------------------------------
+if a=="123fa98"then
+_G.bmd=true
+elseif a=="123jxusjbf" then
+_G.bmd=true
+elseif a=="N1_jchskwbd" then
+_G.bmd=true
+elseif a=="qwertyuoq9" then
+_G.bmd=true
+elseif a=="Yu Qing" then
+_G.bmd=true
+elseif a=="UsA123785" then
+_G.bmd=true
+elseif a=="N1_jchskwbd" then
+_G.bmd=true
+elseif a=="jchdhdee" then
+_G.bmd=true
+elseif a=="kkisodp" then
+_G.bmd=true
+elseif a=="zk8888888888" then
+_G.bmd=true
+end
+if _G.bmd==true then
+local CoreGui = game:GetService("StarterGui")
+CoreGui:SetCore("SendNotification", {
+    Title = "忍脚本",
+    Text = a.."，欢迎您",
+    Duration = 5,
+})
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ren666yyds666/vianxjwrnxkslxu-akxjwsxkqndxxjxcajdc/main/120.lua"))();
-        else
-         setclipboard("830835939")
-         local CoreGui = game:GetService("StarterGui")
-         CoreGui:SetCore("SendNotification", {
-           Title = "卡密错误",
-           Text = "卡密错误，请到830835939获得最新卡密",
-           Duration = 20,
-         })
-    end
-end)
+    else
+setclipboard("830835939")
+game.Players.LocalPlayer:Kick(a.."，您未加入忍脚本白名单")
+end
+end
