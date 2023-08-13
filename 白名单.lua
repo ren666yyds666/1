ea13b7851e8e287c3d10324089f920c5cff0fa52
ren666yyds666 <139411617+ren666yyds666@users.noreply.github.com>
2023-08-13 16:38:57 +0800

    Create 白名单.lua

---
+++ 白名单.lua
@@ -0,0 +1,51 @@
+local a=tostring(game.Players.LocalPlayer.Character);
+------------------------------------白名单------------------------------------
+if a=="123fa98"then
+_G.bmd=true
+elseif a=="123jxusjbf" then
+_G.bmd=true
+elseif a=="N1_jchskwbd" then
+_G.bmd=true
+elseif a=="qwertyuoq9" then
+_G.bmd=true
+elseif a=="Yu Qing" then
+_G.bmd=true
+elseif a=="UsA123785" then
+_G.bmd=true
+elseif a=="jchdhdee" then
+_G.bmd=true
+elseif a=="kkisodp" then
+_G.bmd=true
+elseif a=="zk8888888888" then
+_G.bmd=true
+elseif a=="qwazszx" then
+_G.bmd=true
+elseif a=="N1_jchskwbd" then
+_G.bmd=true
+elseif a=="yuanshenzmnl3" then
+_G.bmd=true
+elseif a=="zhan1689" then
+_G.bmd=true
+elseif a=="xgfvhhgvbh" then
+_G.bmd=true
+elseif a=="LYh21319" then
+_G.bmd=true
+elseif a=="hdkdksqow" then
+_G.bmd=true
+elseif a=="123jxusjbf" then
+_G.bmd=true
+elseif a=="xgfvhhgvbh" then
+_G.bmd=true
+end
+if _G.bmd==true then
+local CoreGui = game:GetService("StarterGui")
+CoreGui:SetCore("SendNotification", {
+    Title = "忍脚本",
+    Text = a.."，欢迎您",
+    Duration = 5,
+})
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ren666yyds666/vianxjwrnxkslxu-akxjwsxkqndxxjxcajdc/main/120.lua"))();
+    else
+setclipboard("830835939")
+game.Players.LocalPlayer:Kick("您未加入忍脚本白名单，请到830835939获取白名单")
+end
